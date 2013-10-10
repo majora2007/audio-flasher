c7f89b33e8d5d4839a61aef49c81e14af3603d22
majora2007 <josephmajora@gmail.com>
2013-10-10 15:29:34 -0700

    Inital commit.

---
+++ AudioFlasher/FlasherWindow.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Platform;
+
+namespace AudioFlasher
+{
+    class FlasherWindow : GameWindow
+    {
+        #region Constructor
+        public FlasherWindow()
+            : base( 800, 600 )
+        {
+
+        }
+        #endregion Constructor
+
+        #region OnLoad
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            Color4 color = Color4.MidnightBlue;
+            GL.ClearColor( color.R, color.G, color.B, color.A );
+            GL.Enable( EnableCap.DepthTest );
+        }
+        #endregion OnLoad
+
+        protected override void OnResize( EventArgs e )
+        {
+            GL.Viewport( 0, 0, Width, Height );
+        }
+
+        protected override void OnUpdateFrame( OpenTK.FrameEventArgs e )
+        {
+            if ( Keyboard[OpenTK.Input.Key.Escape] )
+            {
+                this.Exit();
+                return;
+            }
+        }
+
+        protected override void OnRenderFrame( OpenTK.FrameEventArgs e )
+        {
+            GL.Clear( ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit );
+
+            GL.Color3( 0.0f, 0.0f, 1.0f );
+
+            this.SwapBuffers();
+        }
+
+
+    }
+}
---
+++ AudioFlasher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioFlasher
+{
+    class Program
+    {
+        [STAThread]
+        public static void Main( string[] args )
+        {
+            using ( FlasherWindow flashWindow = new FlasherWindow() )
+            {
+                //Utilities.SetWindowTitle( flashWindow );
+                flashWindow.Run( 30.0, 0.0 );
+            }
+        }
+    }
+}
