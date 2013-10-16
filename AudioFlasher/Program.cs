f4e11ddbfbb297aa98fd3ff5d1c0bfbef21f6f5c
majora2007 <josephmajora@gmail.com>
2013-10-15 20:20:50 -0700

    Refactored the code. For some reason, the second elapse code is being executed 4 times per second.

--- AudioFlasher/Program.cs
+++ AudioFlasher/Program.cs
@@ -13,8 +13,8 @@ namespace AudioFlasher
 {
     class Program
     {
-        const string filename = "C:/Users/jvmilazz/Desktop/zero-ten.wav";
-        //static readonly string filename = Path.Combine( Path.Combine( "Data", "Audio" ), "the_ring_that_fell.wav" );
+        const string filename = "C:/Users/jvmilazz/Desktop/one.wav";
+        //static readonly string filename = Path.Combine( Path.Combine( "Data", "Audio" ), "one.wav" );
         const int BUFFER_SIZE = (int) (0.5 * 44100);
         const int BUFFER_COUNT = 4;
 
@@ -35,43 +35,38 @@ namespace AudioFlasher
                 byte[] sound_data = Playback.LoadWave( File.Open( filename, FileMode.Open ), out channels, out bits_per_sample, out sample_rate );
                 AL.BufferData( buffer, Playback.GetSoundFormat( channels, bits_per_sample ), sound_data, sound_data.Length, sample_rate );
 
+
+
                 Trace.WriteLine( "Bits per Sample: " + bits_per_sample );
                 Trace.WriteLine( "Sample Rate: " + sample_rate );
 
                 // Every 2 bytes is equal to one sample. The reason for the 4 is because the audio file has sample for left ear, then sample for right ear. These should count as 1 sample.
                 int seconds = (sound_data.Length/ sample_rate) / 4;
                 Trace.WriteLine( "There should be " + seconds + " seconds of sound to play." );
-                int pulseRate = 0, pulseCount = 0;
+
+                int pulseCount = 0;
                 for ( int i = 0; i < sound_data.Length; i+=4 )
                 {
-                    int currentSecond = 0;
-                    if (i > 0) currentSecond = (i / sample_rate) / 4;
+                    int currentSecond = calculateSecond( i, sample_rate );
                     int leftSample = sound_data[i] + sound_data[i + 1];
                     int rightSample = sound_data[i + 2] + sound_data[i + 3];
-
                     int sample = leftSample + rightSample;
-                    //Console.WriteLine( "Byte at second {0} is {1}", currentSecond, sample );
 
-                    // sample rate * 2 because we have 2 bytes per sample
-                    /*if ( i > 1 && (i*2) / (sample_rate) == 1 )
-                    {
-                        Console.WriteLine( "There were {0} bytes for 1 second.", i );
-                        break;
-                    }*/
-
-                    if ( sample >= 255 )
+                    if ( sample == 1020 ) // 1020 is always max per second
                     {
                         pulseCount++;
                     }
 
-                    if ( i > 1 && (i*2) % sample_rate == 0 && pulseCount > 0 )
+                    // BUG: TODO: This is executed twice for every second and the second execution is wrong
+                    if ( hasSecondElapsed( i, sample_rate ) ) 
                     {
-                        pulseRate = sample_rate / pulseCount;
-                        Console.WriteLine( "There are {0} pulses for the {1}th second.", pulseRate, currentSecond );
+                        Trace.WriteLine( "Second " + currentSecond + " elapsed at index " + i );
+                        Console.WriteLine( "There are {0} pulses for the {1}th second.", pulseCount, currentSecond );
                         pulseCount = 0;
+                        if ( currentSecond == 10 ) break;
                     }
-                    
                 }
+                
 
                 AL.Source( source, ALSourcei.Buffer, buffer );
                 AL.SourcePlay( source );
@@ -98,6 +93,19 @@ namespace AudioFlasher
             {
                 flashWindow.Run( 30.0, 0.0 );
             }
+        }
+
+        private static int calculateSecond( int i, int sampleRate  )
+        {
+            if ( i > 0 ) return (i / sampleRate) / 4;
+            else return 0;
+        }
+
+        // sample rate * 2 because we have 2 bytes per sample
+        private static bool hasSecondElapsed( int i, int sampleRate )
+        {
+            if ( i == 0 ) return false;
+            else return (i * 2) % sampleRate == 0;
         }
     }
 }
